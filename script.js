31d47aedc6387af4b5872e57ea9653e1acae509b
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-04-09 13:59:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your code here
+// script.js
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var blockquote = document.querySelector('blockquote');
+
+    blockquote.innerHTML = "New Delhi is the capital of India and an administrative district of NCT Delhi.";
+});
